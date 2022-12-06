f642992d192464ac556aeb4d4f459d89eb2fe31a
ZHartem21 <zhartem21@gmail.com>
2022-12-06 21:01:53 +0300

    Move settings to environment file

--- project/settings.py
+++ project/settings.py
@@ -1,21 +1,26 @@
 import os
 
+from dotenv import load_dotenv
+
+load_dotenv('db_settings.env')
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'HOST': 'checkpoint.devman.org',
-        'PORT': '5434',
-        'NAME': 'checkpoint',
-        'USER': 'guard',
-        'PASSWORD': 'osim5',
+        'ENGINE': os.environ['ENGINE'],
+        'HOST': os.environ['HOST'],
+        'PORT': os.environ['PORT'],
+        'NAME': os.environ['NAME'],
+        'USER': os.environ['USER'],
+        'PASSWORD': os.environ['PASSWORD'],
     }
 }
 
 INSTALLED_APPS = ['datacenter']
 
-SECRET_KEY = 'REPLACE_ME'
+SECRET_KEY = os.environ['SECRET_KEY']
 
-DEBUG = True
+DEBUG = os.environ['DEBUG']
 
 ROOT_URLCONF = 'project.urls'
 
